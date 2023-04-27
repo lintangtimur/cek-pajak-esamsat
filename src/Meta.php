47ba49f80b9bb5823b6e20c7ac4e85a55a7ff70e
lintangtimur <lintangtimur915@gmail.com>
2023-04-27 11:50:44 +0700

    cek pajak endpoint

--- src/Meta.php
+++ src/Meta.php
@@ -7,7 +7,7 @@ class Meta
     /**
      * Sementara backend belum siap, masih di local
      */
-    const BASE_URL = "localhost:8000/";
+    const BASE_URL = "https://stelin.xyz/";
     const SIGNKEY = "lintangtimur";
     const ALGO = "sha256";
 }
